db1fb258712171b50036cf3087f611df3e5dad9e
Mehdi Mounsif <mounsif.mehdi@gmail.com>
2021-09-21 09:50:52 +0200

    model predictions

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import numpy as np 
-import tensorflow as tf 
-from tensorflow import keras
+# import tensorflow as tf 
+# from tensorflow import keras
 import librosa 
 import matplotlib.pyplot as plt 
 from sklearn.preprocessing import minmax_scale
@@ -14,8 +14,9 @@ def load_model(path):
 
 class AInimalsModel:
 
-    def __init__(self, path = None): 
+    def __init__(self, path = None, nb_classes = 5): 
         self.m = load_model(path)
+        self.nb_classes = nb_classes
 
     def __call__(self, x_im): 
         preds = self.m(x_im)
@@ -28,11 +29,21 @@ class AInimalsModel:
         db = librosa.power_to_db(mel)
         db = minmax_scale(db)
         db_array = (np.asarray(db)*255).astype(np.uint8)
-        plt.matshow(db_array, cmap = 'hot')
-        plt.show()
         return db_array
+
+    def get_preds(self, audio_path, start_at, window): 
+        spectro = self.get_spectrogam(audio_path, start_at, window)
+        if self.m is not None: 
+            preds = self.m(x)
+        else: 
+            out = np.random.uniform(0.,1.,size = (self.nb_classes,))
+            preds = np.exp(out)/np.exp(out).sum()
+        
+        print('Best guess: {} - Preds: {}'.format(np.argmax(preds), preds))
+        return preds, np.argmax(preds), spectro
 
 
 if __name__ == "__main__": 
     m = AInimalsModel()
-    m.get_spectrogam(glob.glob('./*.wav')[0],0, 3)
+    #m.get_spectrogam(glob.glob('./*.wav')[0],0, 3)
+    m.get_preds(glob.glob('./*.wav')[0],0, 3)
