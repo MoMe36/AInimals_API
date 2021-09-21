d92196f98a3f57cb54a52cfac6cb21a7c3bf277b
cyrillecoq <cyril.lecoq@protonmail.com>
2021-09-21 17:36:04 +0200

    Ajout des paramètres de configuration dans la fonction d'analyse

--- AInimalsProgrammingInterface.py
+++ AInimalsProgrammingInterface.py
@@ -15,6 +15,9 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+        #Set vars from form
+        winSize=request.form['winSizeInput']
+        winStart=request.form['StartPos']
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
@@ -28,25 +31,30 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('analyse_file', name=filename))
+            return redirect(url_for('analyse_file', songfile=filename, windowSize=winSize, windowStart=winStart))
     return '''
     <!doctype html>
     <title>Chant à analyser:</title>
     <h1>Envoyer un fichier son:</h1>
     <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload><br />
+      <label for="file">Fichier à analyser:</label>
+      <input type=file name=file><br />
       <label for="winSizeInput">Taille de la fenêtre glissante (entre 1 and 10):</label>
       <input type="number" name="winSizeInput" min="1" max="10" value="5" oninput="this.form.winSizeRange.value=this.value" />
       <input type="range" name="winSizeRange" min="1" max="10" value="5" oninput="this.form.winSizeInput.value=this.value" /><br />
       <label for="StartPos">Position de départ de la fenêtre:</label>
-      <input type="number" name="StartPos" min="0.0" max="300.0" value="0.0" step="0.1" />
+      <input type="number" name="StartPos" min="0.0" max="300.0" value="0.0" step="0.1" /><br />
+      <input type=submit value=Envoyer>
     </form>
     '''
 
-@app.route('/uploads/<name>')
-def analyse_file(name):
-    return 'Résultats: <name>'
+@app.route('/uploads/<songfile>/<windowSize>/<windowStart>')
+def analyse_file(songfile,windowSize,windowStart):
+    return '''
+    Fichier à traiter: '''+songfile+'''<br />
+    Taille de la fenêtre: '''+windowSize+'''S<br />
+    Début de la fenêtre: '''+windowStart+'''S
+    '''
 
 if __name__ == '__main__':
     app.run()
